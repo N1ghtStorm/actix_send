277658a7662de7ffe444651002be5b23e826a1d8
fakeshadow <24548779@qq.com>
2020-07-02 04:19:33 +0800

    fix blocking for async-std

--- src/util/runtime.rs
+++ src/util/runtime.rs
@@ -9,8 +9,7 @@ macro_rules! spawn {
         $delay_fn: path,
         $interval_fn: path,
         $interval_ty: path,
-        $tick_fn: ident,
-        $spawn_blocking_fn: path
+        $tick_fn: ident
     ) => {
         pub(crate) fn spawn<Fut>(f: Fut)
         where
@@ -31,16 +30,6 @@ macro_rules! spawn {
         pub(crate) async fn tick(interval: &mut $interval_ty) {
             let _ = interval.$tick_fn().await;
         }
-
-        pub async fn spawn_blocking<F, T>(f: F) -> Result<T, ActixSendError>
-        where
-            F: FnOnce() -> T + Send + 'static,
-            T: Send + 'static,
-        {
-            $spawn_blocking_fn(f)
-                .await
-                .map_err(|_| ActixSendError::Blocking)
-        }
     };
 }
 
@@ -51,8 +40,7 @@ spawn!(
     tokio::time::delay_for,
     tokio::time::interval,
     tokio::time::Interval,
-    tick,
-    tokio::task::spawn_blocking
+    tick
 );
 
 #[cfg(feature = "async-std-runtime")]
@@ -62,10 +50,29 @@ spawn!(
     async_std::task::sleep,
     async_std::stream::interval,
     async_std::stream::Interval,
-    next,
-    async_std::task::spawn_blocking
+    next
 );
 
 #[cfg(feature = "async-std-runtime")]
 #[cfg(not(feature = "tokio-runtime"))]
 use futures::StreamExt;
+
+pub async fn spawn_blocking<F, T>(f: F) -> Result<T, ActixSendError>
+where
+    F: FnOnce() -> T + Send + 'static,
+    T: Send + 'static,
+{
+    #[cfg(feature = "tokio-runtime")]
+    #[cfg(not(feature = "async-std-runtime"))]
+    {
+        tokio::task::spawn_blocking(f)
+            .await
+            .map_err(|_| ActixSendError::Blocking)
+    }
+
+    #[cfg(feature = "async-std-runtime")]
+    #[cfg(not(feature = "tokio-runtime"))]
+    {
+        Ok(async_std::task::spawn_blocking(f).await)
+    }
+}
