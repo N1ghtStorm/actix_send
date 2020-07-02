d714f4ffbcc0eaa15d69d213f3a620c3e4ef316d
fakeshadow <24548779@qq.com>
2020-07-02 08:45:39 +0800

    make some change to example

--- examples/blocking.rs
+++ examples/blocking.rs
@@ -1,44 +1,50 @@
+use std::sync::{
+    atomic::{AtomicUsize, Ordering},
+    Arc,
+};
+
 use actix_send::prelude::*;
 use tokio::task::JoinError;
 
 use crate::my_actor::*;
 
-#[tokio::main(threaded_scheduler, core_threads = 12)]
+#[tokio::main]
 async fn main() {
-    let actor = MyActor::create(|| MyActor { state: 0 });
+    let actor = MyActor::create(|| MyActor {
+        state: 0,
+        state2: Arc::new(AtomicUsize::new(0)),
+    });
 
-    let address = actor.build().start();
-
-    let (tx, rx) = futures::channel::oneshot::channel::<()>();
+    // We need to build 2 actors as one actor can handle only one message at a time.
+    let address = actor.build().num(2).start();
 
     let addr = address.clone();
-    tokio::spawn(async move {
+    let f1 = async move {
         let res = addr.send(Message).await;
 
         println!("We got result for Message last\r\nResult is: {:?}\r\n", res);
+    };
 
-        let _ = tx.send(());
-    });
+    let f2 = async move {
+        let res = address.send(Message2).await;
+        println!(
+            "We got result for Message2 first \r\nResult is: {:?}\r\n",
+            res
+        );
+    };
 
-    // We are not blocked by the task;
-
-    let res = address.send(Message2).await;
-
-    println!(
-        "We got result for Message2 first \r\nResult is: {:?}\r\n",
-        res
-    );
-
-    let _ = rx.await;
+    let _ = tokio::spawn(futures::future::join(f1, f2)).await;
 }
 
 #[actor_mod]
 pub mod my_actor {
     use super::*;
+    use std::time::Duration;
 
     #[actor]
     pub struct MyActor {
         pub state: usize,
+        pub state2: Arc<AtomicUsize>,
     }
 
     /*
@@ -64,14 +70,15 @@ pub mod my_actor {
             /*  You ca do some async computation first  */
 
             // just call blocking feature of your runtime directly.
-            tokio::task::spawn_blocking(move || {
-                let mut i = 0usize;
-                for _ in 0..2_000_000 {
-                    i += 1;
-                }
-                i
+            let res = tokio::task::spawn_blocking(move || {
+                std::thread::sleep(Duration::from_secs(1));
+                2
             })
-            .await
+            .await;
+
+            assert_eq!(self.state2.load(Ordering::SeqCst), 1);
+
+            res
 
             /*  Or some async after */
         }
@@ -79,18 +86,27 @@ pub mod my_actor {
 
     #[handler]
     impl Handler for MyActor {
-        fn handle(&mut self, _: Message2) -> usize {
+        fn handle(&self, _: Message2) -> usize {
             /*
                 We marked Message2 as blocking in #[message] attribute
                 So no async code allowed in handle method.
             */
 
-            println!("{}", self.state);
-            let mut i = 0usize;
-            for _ in 0..1_000_000 {
-                i += 1;
-            }
-            i
+            /*
+              ***.  LIMITATION:
+
+                    self here a Clone of MyActor's state.
+                    You can not mutate the state here.
+                    (Unless MyActor is a shareable actor with thread safe smart pointers.)
+
+            */
+
+            // self.state += 1; // This line would cause failure for compile if you uncomment.
+            self.state2.fetch_add(1, Ordering::SeqCst); // state2 of MyActor would be 1 now.
+
+            std::thread::sleep(Duration::from_millis(100));
+
+            1
         }
     }
 }
